0c4e981566bfd2512ae2b8108583da7e956c15d6
Bookmarkletdood <132941551+Bookmarkletdood@users.noreply.github.com>
2023-06-21 07:22:45 -0400

    Update IFramR.js

--- IFramR/IFramR.js
+++ IFramR/IFramR.js
@@ -3,7 +3,7 @@ let Name
 document.getElementById("Enter-Name-Submit").onclick = function(){
 
 	Name = 
-	document.getElementById("Enter-Name").value;
+	document.getElementById("Enter-Name-Input").value;
 	
 	document.getElementById("Enter-Name-Header").innerHTML = "Welcome to IFramR " + Name;
 }
